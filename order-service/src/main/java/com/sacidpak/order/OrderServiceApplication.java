083f02a0663c22f80b8a3313aed08594eb5c51d5
sacidpak <sacid.pak@id3.com.tr>
2024-11-04 22:17:37 +0300

    outbox retry added.

--- order-service/src/main/java/com/sacidpak/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/sacidpak/order/OrderServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
+@EnableScheduling
 @EnableFeignClients(basePackages = "com.sacidpak.clients")
 @SpringBootApplication(
         scanBasePackages = {
